afbcb16b8449c5adbdd83883b6058f563feef8c7
sirethorn13 <113495374+sirethorn13@users.noreply.github.com>
2022-10-12 11:29:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,32 @@
-<!DOCTYPE html>
-<html>
+# MyResume
+Personal CV/RESUME/PORTFOLIO site template. This template allows anyone to create own responsive resume site in a minute. The template is very simple, user-friendly & responsive.
 
-<head>
-	<title>Welcome to Resume Template</title>
-	<link rel="stylesheet" type="text/css" href="template.css">
-</head>
+![resume](https://user-images.githubusercontent.com/30004820/28014454-4cb14e4e-658a-11e7-9ed0-cde5180fbcc9.png)
 
-<body>
-<section class="" ="sec1">
-	<div class="cdiv1">
-	 <a href="index.html">RANDY PETTERSON</a>
-	 <a href="portfolio.html">PORTFOLIO</a>
-	 <a href="contact.php">CONTACT</a>
-	 <a href="resume.html">RESUME</a>	
-	</div></section></br>
+### Full Live Demo 
+[MyResume](https://anirbanroot.000webhostapp.com/resume/)
+### Requirements or Tools
+- Html , Css
+- PHP 5.3 or higher recommended 
+- Ability to write .htaccess file for apache
 
+### Installation
+- Upload MyResume template to the directory of your choice.
+- Make required changes according to your need.
+- Navigate to the installation in your browser.
+- Done :)
 
-	 <div class="cdiv2">
-		<img src="images/anirban1.jpg">
-	 </div>
+### To-Do  list
+- Add footer section
+- Add social link buttons
 
-	 <div class="cdiv3">
-		<p>A "Me in 30 Seconds" statement is a simple way to present<br>
-		 to someone else a balanced understanding of who you are.<br>
-		 It piques the interest of a listener who invites you to<br>
-		 "Tell me a little about yourself," and it provides a brief<br>
-		  and compelling answer to the question<br> "Why should I hire you"?</p>
-	 </div><br>
+### Developers team:
+- Front-end : [Austin Enas](https://www.facebook.com/emperor.austi)
+- Back-end : [Anirban Dutta](https://github.com/anirbandutta9)
 
-	 <div class="cdiv4">
-	 	<p>
-	 		"My name is Randy Patterson, and I'm currently looking for a job in youth services. I have 10 years of experience working with youth agencies. I have a bachelor's degree in outdoor education. I raise money, train leaders, and organize units. I have raised over $100,000 each of the last six years. I consider myself a good public speaker, and I have a good sense of humor. "Who do you know who works with youth?"
-	 	</p>
-	 	<p>
-	 		"My name is Lucas Martin, and I enjoy meeting new people and finding ways to help them have an uplifting experience. I have had a variety of customer service opportunities, through which I was able to have fewer returned products and increased repeat customers, when compared with co-workers. I am dedicated, outgoing, and a team player. Who could I speak with in your customer service department about your organization's customer service 
-	 	</p>
-	 </div>
-<section>
-	
-</section>
+### Issues
 
-</body>
-</html>
+Please log any issues found in the repository 
+
+### License
+MyResume is released under the MIT License
