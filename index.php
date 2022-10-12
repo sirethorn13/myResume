8dafb03d35ca143ea3b6a7aba2f716e692b9c750
sirethorn13 <113495374+sirethorn13@users.noreply.github.com>
2022-10-12 11:32:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, World!</title>
+    <title>PHP Syntax</title>
 </head>
 <body>
-        <h1><?php echo 'Hello, World!'; ?></h1>
+        <h1><?php echo 'PHP Syntax'; ?></h1>
 </body>
 </html>
