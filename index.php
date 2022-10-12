04c32587b3fcb1cf38d6575ee024008a63aec573
sirethorn13 <113495374+sirethorn13@users.noreply.github.com>
2022-10-12 11:21:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
-# Draco
-A free PSD & HTML resume template.
-See the demo [here](http://draco.afnizarnur.com/) and download the latest release [here](https://github.com/afnizarnur/draco/releases).
-![Draco](https://cloud.githubusercontent.com/assets/4648648/26038614/72fe3772-3936-11e7-8f88-3781aea67bdc.png)
+<!DOCTYPE html>
+<html>
 
-## License
-<a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />
+<head>
+	<title>Welcome to Resume Template</title>
+	<link rel="stylesheet" type="text/css" href="template.css">
+</head>
 
-Draco is licensed under Creative Commons [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/). Which means that you are free to copy and redistribute the material in any medium or format and remix, transform, and build upon the material.
+<body>
+<section class="" ="sec1">
+	<div class="cdiv1">
+	 <a href="index.html">RANDY PETTERSON</a>
+	 <a href="portfolio.html">PORTFOLIO</a>
+	 <a href="contact.php">CONTACT</a>
+	 <a href="resume.html">RESUME</a>	
+	</div></section></br>
 
-This template is designed and developed by Afnizar Nur Ghifari.
+
+	 <div class="cdiv2">
+		<img src="images/anirban1.jpg">
+	 </div>
+
+	 <div class="cdiv3">
+		<p>A "Me in 30 Seconds" statement is a simple way to present<br>
+		 to someone else a balanced understanding of who you are.<br>
+		 It piques the interest of a listener who invites you to<br>
+		 "Tell me a little about yourself," and it provides a brief<br>
+		  and compelling answer to the question<br> "Why should I hire you"?</p>
+	 </div><br>
+
+	 <div class="cdiv4">
+	 	<p>
+	 		"My name is Randy Patterson, and I'm currently looking for a job in youth services. I have 10 years of experience working with youth agencies. I have a bachelor's degree in outdoor education. I raise money, train leaders, and organize units. I have raised over $100,000 each of the last six years. I consider myself a good public speaker, and I have a good sense of humor. "Who do you know who works with youth?"
+	 	</p>
+	 	<p>
+	 		"My name is Lucas Martin, and I enjoy meeting new people and finding ways to help them have an uplifting experience. I have had a variety of customer service opportunities, through which I was able to have fewer returned products and increased repeat customers, when compared with co-workers. I am dedicated, outgoing, and a team player. Who could I speak with in your customer service department about your organization's customer service 
+	 	</p>
+	 </div>
+<section>
+	
+</section>
+
+</body>
+</html>
