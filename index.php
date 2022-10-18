f96808731fddd7f10f4132d7cf58409e62869800
sirethorn13 <113495374+sirethorn13@users.noreply.github.com>
2022-10-19 00:38:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Vachira Poomsaka CV</title>
+<title>Sirethorn jaiuifui</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
@@ -30,11 +30,11 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
           </div>
         </div>
         <div class="w3-container">
-          <p><i class="fa fa-person fa-fw w3-margin-right w3-large w3-text-aqua"></i>Vachira Poomsaka</p>
+          <p><i class="fa fa-person fa-fw w3-margin-right w3-large w3-text-aqua"></i>Sirethorn Jaiuifui</p>
           <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-aqua"></i>Student</p>
           <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-aqua"></i>Maha Sarakham, Thailand</p>
-          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-aqua"></i>62010914609@msu.ac.th</p>
-          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-aqua"></i>0941971793</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-aqua"></i>sirethorn.jai@msu.ac.th</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-aqua"></i>0839316709</p>
           <hr>
           <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-aqua"></i>Skills</b></p>
           <p>Python</p>
@@ -59,19 +59,15 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
           <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-aqua"></i>Languages</b></p>
                      <p>Thai</p>
           <div class="w3-light-grey w3-round-xlarge">
-            <div class="w3-round-xlarge w3-center w3-aqua" style="height:24px;width:90%">90%</div>
+            <div class="w3-round-xlarge w3-center w3-aqua" style="height:24px;width:90%">100%</div>
           </div>
           <p>English</p>
           <div class="w3-light-grey w3-round-xlarge">
-            <div class="w3-round-xlarge w3-center w3-aqua" style="height:24px;width:50%">50%</div>
+            <div class="w3-round-xlarge w3-center w3-aqua" style="height:24px;width:50%">60%</div>
           </div>
           <p>Lao</p>
           <div class="w3-light-grey w3-round-xlarge">
-            <div class="w3-round-xlarge w3-center w3-aqua" style="height:24px;width:30%">30%</div>
-          </div>
-          <p>Japan</p>
-          <div class="w3-light-grey w3-round-xlarge">
-            <div class="w3-round-xlarge w3-center w3-aqua" style="height:24px;width:10%">10%</div>
+            <div class="w3-round-xlarge w3-center w3-aqua" style="height:24px;width:30%">100%</div>
           </div>
           <br>
         </div>
@@ -90,38 +86,12 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
         <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-aqua"></i>Education</h2>
         <div class="w3-container">
           <h5 class="w3-opacity"><b>Business Information Technology, Mahasarakham University</b></h5>
-          <h6 class="w3-text-aqua"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2019-2022</h6>
+          <h6 class="w3-text-aqua"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2015-2022</h6>
           <p>Studying</p>
           <hr>
         </div>
       </div>
-      
-      
-       <div class="w3-container w3-card w3-white">
-        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-book fa-fw w3-margin-right w3-xxlarge w3-text-aqua"></i>Academic Work (Nothing)</h2>
-      </div>
-      
-      
-       <div class="w3-container w3-card w3-white">
-        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-arrow-up fa-fw w3-margin-right w3-xxlarge w3-text-aqua"></i>Self-Development</h2>
-        <div class="w3-container">
-          <h5 class="w3-opacity"><b>
-สุดยอดแนวคิดธุรกิจยุคดิจิทัล |The Ultimate Business Concepts in Digital Era </b></h5>
-          <h6 class="w3-text-aqua"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2020</h6>      
-          <hr>
-        </div>
-        
-         <div class="w3-container">
-          <h5 class="w3-opacity"><b> การสร้างสรรค์เนื้อหาด้วยข้อมูล (Data) เพื่อเพิ่มคุณค่าขององค์กร | Content Creation in Digital Organization </b></h5>
-          <h6 class="w3-text-aqua"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2020</h6>      
-          <hr>
-        </div>
-        <img src="../Pictures/Capture.PNG" style="width:95%">
-        <img src="../Pictures/222.PNG" style="width:95%">
-      </div>
-    <!-- End Right Column -->
-    </div>
-    
+     
   <!-- End Grid -->
   </div>
   
